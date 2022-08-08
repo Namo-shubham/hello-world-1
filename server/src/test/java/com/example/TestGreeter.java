cff3f92d5ab6232b2bca2250f2e2576ecf7885e8
Namo-shubham <110653616+Namo-shubham@users.noreply.github.com>
2022-08-08 23:51:23 +0530

    Update TestGreeter.java
    source option 6 deleted

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -3,7 +3,6 @@ package com.example;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.containsString;
